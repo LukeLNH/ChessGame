e5e4058027ee3cda1e22b03bba6e25e869b240a8
kapoooya2001 <55564765+kapoooya2001@users.noreply.github.com>
2020-07-07 17:44:53 +0700

    Updates

---
+++ precache-manifest.e53c082f2eb7deafae1d8253e8754284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc680f91f6372d29b353801a3e66471",
+    "url": "/ChessGame/index.html"
+  },
+  {
+    "revision": "7b174f710cc4c7813401",
+    "url": "/ChessGame/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2ce9017bfd9fca7110b",
+    "url": "/ChessGame/static/css/main.2dd98742.chunk.css"
+  },
+  {
+    "revision": "7b174f710cc4c7813401",
+    "url": "/ChessGame/static/js/2.7fb0fadb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChessGame/static/js/2.7fb0fadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ce9017bfd9fca7110b",
+    "url": "/ChessGame/static/js/main.82bc4ed4.chunk.js"
+  },
+  {
+    "revision": "8e4b60447fe901cff03d",
+    "url": "/ChessGame/static/js/runtime-main.1b6d7b72.js"
+  }
+]);
